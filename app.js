589376dd38bdd32d67971ff4e708eae8f57ea3fd
Rodrigo Lezana <75607037+kunx1@users.noreply.github.com>
2023-03-07 21:07:13 -0300

    primer comit

---
+++ app.js
@@ -0,0 +1,6 @@
+const button = document.querySelector('.button')
+const content_info = document.querySelector('.content_info')
+button.addEventListener('click', ()=>{
+    content_info.classList.toggle('activo')
+    
+})
